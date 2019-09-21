f52f107457e69921f33f3b24243685da1192a008
Кирилл Мехонцев <poalzegzaon@gmail.com>
2019-09-21 13:30:45 +0300

    all types Java

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alloha world");//sout
+        int number = 0; //4b
+        float num = 7.45f;//8b
+        short numberr = 234;//2b
+        boolean flag = false;
+        byte numbber = 4;
+        double dnum = 467.7;//8b
+        long numm = 44525434;//8b
+        char word = 'A';
+
+
+    }
+}
