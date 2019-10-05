b8287559b88654a04b2f27e65d73db540121839a
BobableBobBobinski <polinaim888@gmail.com>
2019-10-05 06:15:30 +0300

    Works but there's no test yet

--- src/main/java/homework2/Respectful.java
+++ src/main/java/homework2/Respectful.java
@@ -7,20 +7,12 @@ public class Respectful {
         Scanner sc = new Scanner(System.in);
         String saying = sc.next();
         String word = "Hello";
-        int m = -10;
-        while (m != 0)
-            if (saying.startsWith(word)) {
-                System.out.println("Do you want to exit?");
-                m = sc.nextInt();
-                if (m == 0) {
-                    System.exit(0);
-                } else {
-                    continue;
-                }
-            } else {
-                System.out.println("You're rude, go away >:/ ");
-                System.exit(0);
-            }
+        if (saying.startsWith(word)) {
+            System.out.println("Nice :) ");
+        } else {
+            System.out.println("You're rude, go away >:/ ");
+        } System.out.println("Goodbye");
+            System.exit(0);
     }
 }
 
