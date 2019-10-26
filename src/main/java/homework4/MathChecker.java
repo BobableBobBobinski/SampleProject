20e6faaf5d86b6f39e51d0d96c16aa22f724486d
BobableBobBobinski <polinaim888@gmail.com>
2019-10-26 11:04:40 +0300

    Arithmetic and Geometric

---
+++ src/main/java/homework4/MathChecker.java
@@ -0,0 +1,39 @@
+package homework4;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MathChecker {
+    public static void main(String[] args) {
+
+        try(FileReader reader = new FileReader("src\\main\\resources\\numbersHomework")) {
+            Scanner sc = new Scanner(reader);
+            List<Float> list = new ArrayList<>();
+            while (sc.hasNext()) {
+               float a = sc.nextFloat();
+               list.add(a);
+            }
+            System.out.println("Numbers: " + list);
+            for (int i = 0; i < list.size(); i++) {
+                if (i != list.size() - 1) {
+                    if (list.get(i + 1) - list.get(i) == list.get(i + 2) - list.get(i + 1)) {
+                        System.out.println("It's arithmetic progression");
+                        break;
+                    }
+                    if (list.get(i + 1) / list.get(i) == list.get(i + 2) / list.get(i + 1)) {
+                        System.out.println("It's geometric progression");
+                        break;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error!" + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("ERROR!\n");
+        }
+    }
+}
