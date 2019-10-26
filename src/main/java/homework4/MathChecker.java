9af9dbf78ed737493b8b73c9008ad7bbd14c203d
BobableBobBobinski <polinaim888@gmail.com>
2019-10-26 11:19:28 +0300

    Gives outOfBound exception when it's neither arithmetic or geometric progression

--- src/main/java/homework4/MathChecker.java
+++ src/main/java/homework4/MathChecker.java
@@ -19,12 +19,12 @@ public class MathChecker {
             }
             System.out.println("Numbers: " + list);
             for (int i = 0; i < list.size(); i++) {
-                if (i != list.size() - 1) {
-                    if (list.get(i + 1) - list.get(i) == list.get(i + 2) - list.get(i + 1)) {
+                if (i != list.size() - 2) {
+                    if (Float.compare((list.get(i + 1) - list.get(i)), (list.get(i + 2) - list.get(i + 1))) == 0) {
                         System.out.println("It's arithmetic progression");
                         break;
                     }
-                    if (list.get(i + 1) / list.get(i) == list.get(i + 2) / list.get(i + 1)) {
+                    if (Float.compare((list.get(i + 1) / list.get(i)), (list.get(i + 2) / list.get(i + 1))) == 0) {
                         System.out.println("It's geometric progression");
                         break;
                     }
